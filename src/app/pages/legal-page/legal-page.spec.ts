372f23dee931f716fcd66d8b51125c3e8a654917
Shiirel <kassilissal@gmail.com>
2026-03-16 11:58:55 +0100

    part 8.2

---
+++ src/app/pages/legal-page/legal-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LegalPage } from './legal-page';
+
+describe('LegalPage', () => {
+  let component: LegalPage;
+  let fixture: ComponentFixture<LegalPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LegalPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LegalPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/legal-page/legal-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-page',
+  imports: [],
+  templateUrl: './legal-page.html',
+  styleUrl: './legal-page.css',
+})
+export class LegalPage {
+
+}
